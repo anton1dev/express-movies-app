fe0421481843320e82c047d3af45f47344e8fd5c
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-04-12 08:31:52 +0300

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
+})
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  request.get(nowPlayingUrl, (err, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+
+    res.render('index', {
+      parsedData: parsedData.results
+    });
+  })
+});
+
+module.exports = router;
