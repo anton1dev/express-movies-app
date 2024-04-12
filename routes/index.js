227d54400ae8e37d448b0ebf9d99765ba2b5821d
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-04-12 13:32:53 +0300

    done movies-app, preparing to deploy

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
 
 const apiBaseUrl = 'http://api.themoviedb.org/3';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w500';
 
 router.use((req, res, next) => {
   res.locals.imageBaseUrl = imageBaseUrl;
@@ -25,4 +25,33 @@ router.get('/', function (req, res, next) {
   })
 });
 
+router.get('/movie/:id', (req, res, next) => {
+  const movieId = req.params.id;
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+
+  request.get(thisMovieUrl, (err, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    res.render('single-movie', {
+      parsedData
+    })
+  })
+});
+
+router.post('/search', (req, res, next) => {
+  const userSearchTerm = encodeURI(req.body.movieSearch);
+  const cat = req.body.cat;
+  const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`;
+
+  request.get(movieUrl, (err, response, movieData) => {
+    let parsedData = JSON.parse(movieData);
+
+    if (cat === 'person') {
+      parsedData.results = parsedData.results[0].known_for;
+    }
+
+    res.render('index', {
+      parsedData: parsedData.results
+    })
+  })
+})
 module.exports = router;
